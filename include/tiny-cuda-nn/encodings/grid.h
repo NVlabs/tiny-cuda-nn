d0639158dd64b5d146d659eb881702304abaaa24
Thomas Müller <thomas94@gmx.net>
2022-01-19 15:30:32 +0100

    Better 32-bit prime numbers in spatial hash

--- include/tiny-cuda-nn/encodings/grid.h
+++ include/tiny-cuda-nn/encodings/grid.h
@@ -69,7 +69,7 @@ __device__ uint32_t fast_hash(const uint32_t pos_grid[N_DIMS]) {
 	// While 1 is technically not a good prime for hashing (or a prime at all), it helps memory coherence
 	// and is sufficient for our use case of obtaining a uniformly colliding index from high-dimensional
 	// coordinates.
-	constexpr uint32_t primes[7] = { 1, 19349663, 83492791, 25165843, 6291469, 12582917, 3145739 };
+	constexpr uint32_t primes[7] = { 1, 2654435761, 805459861, 3674653429, 2097192037, 1434869437, 2165219737 };
 
 	uint32_t result = 0;
 	#pragma unroll
