93dba17c66c223ebc0312534d00d8e405efcb440
Thomas Müller <thomas94@gmx.net>
2022-06-18 08:52:21 +0200

    reduce_sum: fail loudly when invoked from within a CUDA graph

--- include/tiny-cuda-nn/reduce_sum.h
+++ include/tiny-cuda-nn/reduce_sum.h
@@ -149,6 +149,7 @@ float reduce_sum(T* device_pointer, F fun, uint32_t n_elements, cudaStream_t str
 
 	float sum;
 	CUDA_CHECK_THROW(cudaMemcpyAsync(&sum, workspace_data, sizeof(float), cudaMemcpyDeviceToHost, stream));
+	CUDA_CHECK_THROW(cudaStreamSynchronize(stream));
 	return sum;
 }
 
