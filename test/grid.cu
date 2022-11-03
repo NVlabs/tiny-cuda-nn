e13bfe51d2db20c06a48e1c6dd153c235c8df971
Pierre Wilmot <pierre.wilmot@dimensionstudio.co>
2022-11-03 15:08:45 +0000

    Set batch size to granularity

--- test/grid.cu
+++ test/grid.cu
@@ -9,9 +9,16 @@ TEST_CASE("3D All 0", "GRID")
   nlohmann::json config_json = nlohmann::json::parse(config);
   tcnn::GridEncoding<float> *g = tcnn::create_grid_encoding<float>(3, config_json);
 
-  tcnn::GPUMatrix<float> input(g->n_pos_dims(), 1);
-  tcnn::GPUMatrix<float> output(g->padded_output_width(), 1);
+  // Current codebase only accept multiple of tcnn::batch_size_granularity
+  unsigned int batch_size = tcnn::batch_size_granularity;
+  REQUIRE(batch_size == 128);
+  
+  tcnn::GPUMatrix<float> input(g->n_pos_dims(), batch_size);
+  tcnn::GPUMatrix<float> output(g->padded_output_width(), batch_size);
   input.memset(0);
+
+  REQUIRE(input.n_elements() == 3 /* dimensions */ * 128 /* batch size */);
+  REQUIRE(output.n_elements() == 2 /* feture per level */ * 20 /* levels*/ * 128 /* batch size */);
   
   std::unique_ptr<tcnn::Context> c = g->forward(input, &output);
 
