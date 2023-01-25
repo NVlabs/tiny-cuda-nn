327b3699d8aab6c694ec43b7ee0aeced57301bfb
Thomas Müller <tmueller@nvidia.com>
2023-01-25 11:26:00 +0100

    Update copyright to 2023

--- scripts/gen_sh.py
+++ scripts/gen_sh.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2020-2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2020-2023, NVIDIA CORPORATION.  All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without modification, are permitted
 # provided that the following conditions are met:
