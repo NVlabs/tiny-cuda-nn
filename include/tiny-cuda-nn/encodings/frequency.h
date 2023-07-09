6af6a9ff67b79f6e4ebce0f0a2f8af2a813840d0
Thomas Müller <thomas94@gmx.net>
2023-07-09 09:27:21 +0200

    `FrequencyEncoding`: remove confusing comment

--- include/tiny-cuda-nn/encodings/frequency.h
+++ include/tiny-cuda-nn/encodings/frequency.h
@@ -61,21 +61,9 @@ __global__ void frequency_encoding(
 	const uint32_t i = encoded_index / fan_out;
 	const uint32_t j = encoded_index - i * fan_out;
 
-	/* Layout of outputs (for each input record):
-	 *     frequency-encoded input dimension 0
-	 *     frequency-encoded input dimension 1
-	 *     frequency-encoded input dimension ...
-	 *     padding (value 1.f)
-	 */
 	if (j >= fan_out_encoded) {
 		data_out(j, i) = 1;
 	} else {
-		/* Layout of encoded features (e.g. when inputs abcd.. are XYZ positions):
-		 *     sin(pi a.x), cos(pi a.x), sin(2pi a.x), cos(2pi a.x), sin(4pi a.x) ...
-		 *     sin(pi a.y), cos(pi a.y), sin(2pi a.y), cos(2pi a.y), sin(4pi a.y) ...
-		 *     sin(pi a.z), cos(pi a.z), sin(2pi a.z), cos(2pi a.z), sin(4pi a.z) ...
-		 *     (padding)
-		 */
 		const uint32_t encoded_input_feature_i = j / (n_frequencies * 2);
 		const uint32_t log2_frequency = (j / 2) % n_frequencies;
 
